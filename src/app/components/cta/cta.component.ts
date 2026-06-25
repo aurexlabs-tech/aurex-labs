db4f28d3537ba57d4a16861782e09ff20066df80
K.Bhuvanesh <127986967+Bhuvi11K@users.noreply.github.com>
2026-06-25 10:56:37 +0530

    changes

--- src/app/components/cta/cta.component.ts
+++ src/app/components/cta/cta.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-cta',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './cta.component.html',
   styleUrl: './cta.component.scss',
 })
